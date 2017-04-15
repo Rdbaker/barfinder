833a3269ede7496124a6b3e39f81044670321b47
Ryan Baker <ryan.da.baker@gmail.com>
2017-04-15 08:52:34 -0400

    Initial commit.

---
+++ barfinder/api/chat.py
@@ -0,0 +1,23 @@
+import json
+
+from flask import Blueprint, request, current_app, jsonify
+
+from barfinder.extensions import csrf_protect
+
+mod = Blueprint('chat', __name__, url_prefix='/api/chat')
+
+
+@mod.route('/messages', methods=['GET'])
+def getmessages():
+    return '', 200
+
+
+@mod.route('/messages', methods=['POST'])
+@csrf_protect.exempt
+def createmessage():
+    message = request.get_json()['body']
+    ai_req = current_app.api_ai.text_request()
+    ai_req.session_id = '1'
+    ai_req.query = message
+    ai_res = ai_req.getresponse()
+    return jsonify(json.loads(ai_res.read())), 200
